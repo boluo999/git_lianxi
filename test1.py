70a91289036ba614f79b64a6d15def9b2e21b0ae
fnyi <15210871712@163.com>
2018-07-04 01:09:00 +0800

    understand how stage works

--- test1.py
+++ test1.py
@@ -11,4 +11,7 @@ for i in range(10):
 # dasfs d
 #
 # ''''# asdfas
-# git
+# # git
+# Git is a distributed version control system.
+# Git is free software distributed under the GPL.
+# Git has a mutable index called stage.
